e9a21c73b33093c1eaffd26dd6301ab2aa171f4b
c-annabel <106787550+c-annabel@users.noreply.github.com>
2023-01-06 21:34:48 +0100

    D022 Build Pong

---
+++ Day022/main.py
@@ -0,0 +1,15 @@
+#Build Pong
+
+#1. Create the screen
+#2. Create and move a paddle
+#3. Create another Paddle
+#4. Create the ball and make it move
+#5. Detect collision with wal and bounce
+#6. Detect collision with paddle
+#7. Detect when paddle misses
+#8. Keep score
+
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.exitonclick()
