13184fd35b154bd4753f833b0bf6b032f6b0aeaa
c-annabel <106787550+c-annabel@users.noreply.github.com>
2023-01-18 22:13:29 +0100

    D030 Exception Errors

--- Day030/main.py
+++ Day030/main.py
@@ -19,25 +19,37 @@
 # else:   Do this if there were no exceptions
 # finally:Do this no matter what happens
 #-----------------------------------------------------#
+#
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["sdfefa"]) #it will fail and go to except if not specified.
+#
+# except FileNotFoundError:
+#     #print("There was an error")
+#     file = open("a_file.txt", "w") #if not existed, it creates.
+#     file.write("Something")
+#
+# # except KeyError:
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+#
+# else: #if all tries suceeded.
+#     content = file.read()
+#     print(content)
+#
+# finally:
+#     # file.close()
+#     # print("File was closed.")
+#     raise KeyError #self-raised error
+#     raise TypeError("message locates") #raise error and message
 
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["sdfefa"]) #it will fail and go to except if not specified.
+height = float(input("Height: "))
+weight = int(input("Weight:"))
 
-except FileNotFoundError:
-    #print("There was an error")
-    file = open("a_file.txt", "w") #if not existed, it creates.
-    file.write("Something")
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters.")
 
-# except KeyError:
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist")
+bmi = weight / height ** 2
+print(bmi)
 
-else: #if all tries suceeded.
-    content = file.read()
-    print(content)
-
-finally:
-    file.close()
-    print("File was closed.")
