10b98aa1a9066bfa412b4b49acb20f1d7854d5f8
c-annabel <106787550+c-annabel@users.noreply.github.com>
2023-02-06 01:29:05 +0100

    Update flight_data.py

--- Day039/flight-deals-public/flight_data.py
+++ Day039/flight-deals-public/flight_data.py
@@ -1,6 +1,6 @@
 class FlightData:
 
-    def __int__(self, price, origin_city, origin_airport, desti_city, desti_airport, out_date, return_date):
+    def __init__(self, price, origin_city, origin_airport, desti_city, desti_airport, out_date, return_date):
         self.price = price
         self.origin_city = origin_city
         self.origin_airport = origin_airport
@@ -8,3 +8,4 @@ class FlightData:
         self.desti_airport = desti_airport
         self.out_date = out_date
         self.return_date = return_date
+
